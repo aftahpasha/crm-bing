380cc7f19ccf6eeb4238200ba1b6492f8e02f7fa
Aftah Pasha <aftahpasha@gmail.com>
2022-12-21 06:21:13 +0700

    initial commit

---
+++ database/migrations/2022_12_20_230846_create_customer_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_kontrak');
+            $table->string('nama_pt');
+            $table->string('deskripsi');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->integer('limit_day');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
