96f085a1ba3dbb1e2925c72639475e82d0593e94
Aftah Pasha <aftahpasha@gmail.com>
2022-12-21 12:48:46 +0700

    commit

---
+++ resources/views/customers/create.blade.php
@@ -0,0 +1,72 @@
+@extends('customers.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New customer</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('customers.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('customers.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nomor Kontrak:</strong>
+                <input type="text" name="no_kontrak" class="form-control" placeholder="Nomor Kontrak">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama PT:</strong>
+                <input type="text" class="form-control" name="nama_pt" placeholder="Nama PT">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Deskripsi:</strong>
+                <input type="text" class="form-control" name="deskripsi" placeholder="Deskripsi">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Start Date:</strong>
+                <input type="text" class="form-control" name="start_date" placeholder="Start Date">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>End Date:</strong>
+                <input type="text" class="form-control" name="end_date" placeholder="End Date">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Limit Day:</strong>
+                <input type="text" class="form-control" name="limit_day" placeholder="Limit Day">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\CustomerController;
+Route::resource('customers', CustomerController::class);
 
 Route::get('/', function () {
     return view('welcome');
